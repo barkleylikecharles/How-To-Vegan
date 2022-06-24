7d66d88bafb37136d0fcecaa2d52b1b0bf5e071d
aidyel <aidayeli@icloud.com>
2022-06-23 21:24:33 -0400

    fix the react bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -39,11 +39,11 @@ function App() {
       <Router>
         <>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route  exact path='/' component={SearchRecipes} />
           <Route  exact path='/' component={SavedRecipes} />
           <Route render={() => <h1 className='display-2'>Wrong Page!</h1>} />
-        </Switch>
+        </Routes>
         </>
       </Router>
     </ApolloProvider>
