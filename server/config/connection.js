089284488ecba346680541c2a2d98c199571cbc2
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-29 15:43:41 -0400

    seeding begin

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv")
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/how-to-vegan', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/how-to-vegan', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
